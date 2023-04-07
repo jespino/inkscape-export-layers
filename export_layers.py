aa0fd8663bd384d00545ce19ca251f4817009286
Martin Antrag <martinantrag@gmail.com>
2023-04-07 13:17:40 +0200

    Update export_layers.py
    Windows Export add
    
    is Making Mockups

--- export_layers.py
+++ export_layers.py
@@ -2,54 +2,76 @@
 
 import sys
 sys.path.append('/usr/share/inkscape/extensions')
+import contextlib
 import inkex
 import os
 import subprocess
 import tempfile
 import shutil
-import copy 
+import copy
+import logging
 
+class write_file ():
+    def __init__(self):
+        self.datei = open("C:/Users/Martin/Desktop/test/export.txt", "w")
+    def write(self,text):
+        #''.join(xs)
+        if type(text) == "str":
+            pass
+        elif type(text) == "int":
+            text = str(text)
+        elif type(text) == "list":
+            text = "".join(text)
+        self.datei.write (str(text))
+        self.datei.write ("\n")
 
+    def close (self):
+        self.datei.close()
+file = write_file()
 class PNGExport(inkex.Effect):
     def __init__(self):
         """init the effetc library and get options from gui"""
         inkex.Effect.__init__(self)
         self.arg_parser.add_argument("--path", action="store", type=str, dest="path", default="~/", help="")
-        self.arg_parser.add_argument('--f', '--filetype', action='store', type=str, dest='filetype', default='jpeg', help='Exported file type')
         self.arg_parser.add_argument("--crop", action="store", type=bool, dest="crop", default=False)
+        self.arg_parser.add_argument("--overwrite", action="store", type=bool, dest="overwrite", default=True)
         self.arg_parser.add_argument("--dpi", action="store", type=float, dest="dpi", default=90.0)
 
     def effect(self):
+        file.write("#")
         output_path = os.path.expanduser(self.options.path)
         curfile = self.options.input_file
         layers = self.get_layers(curfile)
-        counter = 1
+        if not os.path.exists(os.path.join(output_path)):
+            os.makedirs(os.path.join(output_path))
+        file.write(layers)
+        for i in range(len(layers)):
+            file.write("#"+str(i))
+            for ii in range(len(layers)-i-1,-1,-1): # find background
+                if layers[ii][2] == "background":
+                    background = layers[ii]
+                    for ii in range(len(layers)-i-1,-1,-1): # find fixed
+                        if layers[ii][2] == "fixed":
+                            fixed = layers[ii]
+                            for ii in range(len(layers)-i-1,-1,-1): # find export
+                                if layers[ii][2] == "export":
+                                    export = layers[ii]
+                                    show_layer_ids = background[0] + fixed[0] + export[0]
+                                    file.write (show_layer_ids)
+                                    layer_label = export[1] + "_" + fixed[1]
 
-        for (layer_id, layer_label, layer_type) in layers:
-            if layer_type == "fixed":
-                continue
+                                    layer_dest_png_path = os.path.join(output_path,  layer_label + ".png")
 
-            show_layer_ids = [layer[0] for layer in layers if layer[2] == "fixed" or layer[0] == layer_id]
+                                    if os.path.isfile(layer_dest_png_path):
+                                        if self.options.overwrite == False:
+                                            continue
+                                    with _make_temp_directory() as tmp_dir:
+                                        layer_dest_svg_path = os.path.join(tmp_dir, "export.svg")
+                                        self.export_layers(layer_dest_svg_path, show_layer_ids)
 
-            if not os.path.exists(os.path.join(output_path)):
-                os.makedirs(os.path.join(output_path))
-
-            with tempfile.NamedTemporaryFile() as fp_svg:
-                layer_dest_svg_path = fp_svg.name
-                self.export_layers(layer_dest_svg_path, show_layer_ids)
-
-                if self.options.filetype == "jpeg":
-                    with tempfile.NamedTemporaryFile() as fp_png:
-                        self.exportToPng(layer_dest_svg_path, fp_png.name)
-                        layer_dest_jpg_path = os.path.join(output_path, "%s_%s.jpg" % (str(counter).zfill(3), layer_label))
-                        self.convertPngToJpg(fp_png.name, layer_dest_jpg_path)
-                        fp_png.close()
-                else:
-                    layer_dest_png_path = os.path.join(output_path, "%s_%s.png" % (str(counter).zfill(3), layer_label))
-                    self.exportToPng(layer_dest_svg_path, layer_dest_png_path)
-
-            counter += 1
-
+                                        layer_dest_png_path = os.path.join(output_path,  layer_label + ".png")
+                                        self.exportToPng(layer_dest_svg_path, layer_dest_png_path)
+        file.close()
     def export_layers(self, dest, show):
         """
         Export selected layers of SVG to the file `dest`.
@@ -84,8 +106,12 @@ class PNGExport(inkex.Effect):
             elif layer_label.lower().startswith("[export] "):
                 layer_type = "export"
                 layer_label = layer_label[9:]
+            elif layer_label.lower().startswith("[background] "):
+                layer_type = "background"
+                layer_label = layer_label[13:]
             else:
                 continue
+
             layers.append([layer_id, layer_label, layer_type])
 
         return layers
@@ -93,13 +119,19 @@ class PNGExport(inkex.Effect):
     def exportToPng(self, svg_path, output_path):
         area_param = '-D' if self.options.crop else 'C'
         command = "inkscape %s -d %s --export-filename \"%s\" \"%s\"" % (area_param, self.options.dpi, output_path, svg_path)
-        p = subprocess.Popen(command.encode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
         p.wait()
 
-    def convertPngToJpg(self, png_path, output_path):
-        command = "convert \"%s\" \"%s\"" % (png_path, output_path)
-        p = subprocess.Popen(command.encode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        p.wait()
+@contextlib.contextmanager
+def _make_temp_directory():
+    temp_dir = tempfile.mkdtemp(prefix="tmp-inkscape")
+    try:
+        yield temp_dir
+    finally:
+        shutil.rmtree(temp_dir)
 
 
 def _main():
